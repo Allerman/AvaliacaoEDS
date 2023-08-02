7a8b118422cace6a08483fd1fdfef9a22d0c87b4
allerman <allerman3301@gmail.com>
2023-08-02 15:55:18 -0300

    criando PROCEDURE de tranferencia de dados

--- Problema2.sql
+++ Problema2.sql
@@ -2,54 +2,54 @@
 -- dos schemas dos hospitais para a nova tabela de pacientes que você
 -- criou no schema stg_prontuario.
 
--- Opção manual, para inserção “única”:
-INSERT INTO stg_prontuario.PACIENTE (id, nome, dt_nascimento, cpf, nome_mae, dt_atualizacao)
-SELECT id, nome, dt_nascimento, cpf, nome_mae, dt_atualizacao
-FROM stg_hospital_a.PACIENTE;
-
-INSERT INTO stg_prontuario.PACIENTE (id, nome, dt_nascimento, cpf, nome_mae, dt_atualizacao)
-SELECT id, nome, dt_nascimento, cpf, nome_mae, dt_atualizacao
-FROM stg_hospital_b.PACIENTE;
-
-INSERT INTO stg_prontuario.PACIENTE (id, nome, dt_nascimento, cpf, nome_mae, dt_atualizacao)
-SELECT id, nome, dt_nascimento, cpf, nome_mae, dt_atualizacao
-FROM stg_hospital_c.PACIENTE;
-
-
 -- Opção automatizada, se for necessário a adição frequente de novos pacientes dos hospitais ao stg_prontuario:
+
 DELIMITER $$
+
 CREATE PROCEDURE transferir_dados_pacientes()
 BEGIN
 
-	INSERT INTO stg_prontuario.PACIENTE (id, nome, dt_nascimento, cpf, nome_mae, dt_atualizacao)
-	SELECT hA.id, hA.nome, hA.dt_nascimento, hA.cpf, hA.nome_mae, hA.dt_atualizacao
-	FROM stg_hospital_a.PACIENTE hA
-	WHERE NOT EXISTS (
-    	SELECT 1
-    	FROM stg_prontuario.PACIENTE p
-    	WHERE p.id = hA.id
-	);
+    START TRANSACTION;
 
-	INSERT INTO stg_prontuario.PACIENTE (id, nome, dt_nascimento, cpf, nome_mae, dt_atualizacao)
-	SELECT hB.id, hB.nome, hB.dt_nascimento, hB.cpf, hB.nome_mae, hB.dt_atualizacao
-	FROM stg_hospital_b.PACIENTE hB
-	WHERE NOT EXISTS (
-    	SELECT 1
-    	FROM stg_prontuario.PACIENTE p
-    	WHERE p.id = hB.id
-	);
+    -- Inserção dos dados de stg_hospital_a.PACIENTE
+    INSERT INTO stg_prontuario.PACIENTE (id, nome, dt_nascimento, cpf, nome_mae, dt_atualizacao)
+    SELECT hA.id, hA.nome, hA.dt_nascimento, hA.cpf, hA.nome_mae, hA.dt_atualizacao
+    FROM stg_hospital_a.PACIENTE hA
+    WHERE NOT EXISTS (
+        SELECT 1
+        FROM stg_prontuario.PACIENTE p
+        WHERE p.id = hA.id
+    );
+    
+    -- Inserção dos dados de stg_hospital_b.PACIENTE
+    INSERT INTO stg_prontuario.PACIENTE (id, nome, dt_nascimento, cpf, nome_mae, dt_atualizacao)
+    SELECT hB.id, hB.nome, hB.dt_nascimento, hB.cpf, hB.nome_mae, hB.dt_atualizacao
+    FROM stg_hospital_b.PACIENTE hB
+    WHERE NOT EXISTS (
+        SELECT 1
+        FROM stg_prontuario.PACIENTE p
+        WHERE p.id = hB.id
+    );
 
-	INSERT INTO stg_prontuario.PACIENTE (id, nome, dt_nascimento, cpf, nome_mae, dt_atualizacao)
-	SELECT hC.id, hC.nome, hC.dt_nascimento, hC.cpf, hC.nome_mae, hC.dt_atualizacao
-	FROM stg_hospital_c.PACIENTE hC
-	WHERE NOT EXISTS (
-    	SELECT 1
-    	FROM stg_prontuario.PACIENTE p
-    	WHERE p.id = hC.id
-	);
+    -- Inserção dos dados de stg_hospital_c.PACIENTE
+    INSERT INTO stg_prontuario.PACIENTE (id, nome, dt_nascimento, cpf, nome_mae, dt_atualizacao)
+    SELECT hC.id, hC.nome, hC.dt_nascimento, hC.cpf, hC.nome_mae, hC.dt_atualizacao
+    FROM stg_hospital_c.PACIENTE hC
+    WHERE NOT EXISTS (
+        SELECT 1
+        FROM stg_prontuario.PACIENTE p
+        WHERE p.id = hC.id
+    );
+
+    -- Confirmação
+    COMMIT;
+    
+    -- Log de sucesso
+    SELECT 'Dados transferidos com sucesso!' AS log_message;
+
 END $$
 DELIMITER ;
 
+
 -- Procedure criada, basta chama-lá com: 
 CALL transferir_dados_pacientes();
-

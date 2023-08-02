7eeb5784b1d6c05ca0077c74b802e51035b34bfe
allerman <allerman3301@gmail.com>
2023-08-02 16:00:28 -0300

    criando as tabelas atendimento e diagnostico

--- Problema7.sql
+++ Problema7.sql
@@ -1,35 +1,38 @@
 -- Dado que um atendimento médico pode ter vários diagnósticos, como
 -- você modelaria essas duas tabelas no banco de dados de staging?
 
+-- Tabela de ATENDIMENTO
 CREATE TABLE stg_prontuario.ATENDIMENTO (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  tipo_atendimento CHAR(2),
+  tipo_atendimento CHAR(1),
   detalhe_atendimento VARCHAR(255)
 );
 
+-- Tabela de DIAGNOSTICO
 CREATE TABLE stg_prontuario.DIAGNOSTICO (
   id INT AUTO_INCREMENT PRIMARY KEY,
   descricao VARCHAR(255),
-  cod_CID VARCHAR(10)
+  cod_CID CHAR(4)
 );
 
--- Relacionando as duas tabelas:
+-- Tabela intermediária para relacionar PACIENTE, ATENDIMENTO e DIAGNOSTICO
 CREATE TABLE stg_prontuario.DIAGNOSTICO_PACIENTE (
   paciente_id INT,
   atendimento_id INT,
   data_atendimento DATE,
-  diagnostico_id INT
+  diagnostico_id INT,
+  PRIMARY KEY (paciente_id, atendimento_id, diagnostico_id) -- Definindo a chave primária composta
 );
 
--- Adicionando as FKs:
+-- Adicionando as FKs
 ALTER TABLE stg_prontuario.DIAGNOSTICO_PACIENTE
-ADD CONSTRAINT fk_diagPaciente_pacientId
+ADD CONSTRAINT fk_diagPaciente_paciente
 FOREIGN KEY (paciente_id) REFERENCES stg_prontuario.PACIENTE(id);
 
 ALTER TABLE stg_prontuario.DIAGNOSTICO_PACIENTE
-ADD CONSTRAINT fk_diagPaciente_atendId
+ADD CONSTRAINT fk_diagPaciente_atendimento
 FOREIGN KEY (atendimento_id) REFERENCES stg_prontuario.ATENDIMENTO(id);
 
 ALTER TABLE stg_prontuario.DIAGNOSTICO_PACIENTE
-ADD CONSTRAINT fk_diagPaciente__diagId
+ADD CONSTRAINT fk_diagPaciente_diagnostico
 FOREIGN KEY (diagnostico_id) REFERENCES stg_prontuario.DIAGNOSTICO(id);

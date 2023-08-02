f1a882ded69526b1e60e1fe8a08d7a994129c25a
allerman <allerman3301@gmail.com>
2023-08-02 15:57:28 -0300

    contando grupos de dados duplicados mais recentes

--- Problema4.sql
+++ Problema4.sql
@@ -2,15 +2,16 @@
 -- uma consulta em SQL que retorne, para cada conjunto de pacientes
 -- repetidos, somente o que tem a data de atualização mais recente.
 
-SELECT p.nome, p.dt_nascimento, p.cpf, p.nome_mae, p.dt_atualizacao
-FROM stg_prontuario.PACIENTE p
-INNER JOIN (
+WITH duplicatas AS (
     SELECT nome, dt_nascimento, cpf, nome_mae, MAX(dt_atualizacao) AS max_data_atualizacao
     FROM stg_prontuario.PACIENTE
     GROUP BY nome, dt_nascimento, cpf, nome_mae
     HAVING COUNT(*) > 1
-) AS duplicatas ON p.nome = duplicatas.nome
-                AND p.dt_nascimento = duplicatas.dt_nascimento
-                AND p.cpf = duplicatas.cpf
-                AND p.nome_mae = duplicatas.nome_mae
-                AND p.dt_atualizacao = duplicatas.max_data_atualizacao;
+)
+SELECT p.nome, p.dt_nascimento, p.cpf, p.nome_mae, p.dt_atualizacao
+FROM stg_prontuario.PACIENTE p
+INNER JOIN duplicatas d ON p.nome = d.nome
+                             AND p.dt_nascimento = d.dt_nascimento
+                             AND p.cpf = d.cpf
+                             AND p.nome_mae = d.nome_mae
+                             AND p.dt_atualizacao = d.max_data_atualizacao;
